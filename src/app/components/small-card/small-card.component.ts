a6bee85d998f2bcf4d8f0200176dee2084c88fa4
José Bastos <josebezerrabastos@gmail.com>
2023-10-07 22:05:25 -0300

    feat: criado content page com dados dinâmicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  
+  @Input() 
+  photoCover = "";
 
+  @Input()
+  cardTitle:string = "";
+
+  @Input()
+  Id:string = "0";
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,31 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+        "description": "Marvel anuncia um novo homem de ferro com Robert Downey Junior e já tem data para a estreia",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=1400&height=800&format=webp&quality=91&imagick=/wp-content/uploads/2021/02/homem-de-ferro-tony-divulgacao.jpg",
+
+    },
+    {
+        "id": "2",
+        "title": "BATALHA DE AZEROTH",
+        "description": "Battle for azeroth já tem dados vazados pelo o WoWHead",
+        "photoCover": "https://sm.ign.com/t/ign_pt/news/b/battle-for/battle-for-azeroth-is-world-of-warcrafts-fastest-selling-exp_3jcc.1200.jpg"
+    },
+    {
+        "id": "3",
+        "title": "JUJUTSU KAISEN 0",
+        "description": "Filme explicará o passado do feiticeiro Yuta Okkotsu. A história diz que ele tão promissor quanto o Satoru Gojo.",
+        "photoCover": "https://criticalhits.com.br/wp-content/uploads/2021/12/jujutsu-kaisen-0-movie-poster-1.jpg"
+    },
+    {
+        "id": "4",
+        "title": "TEM JOGO BOM VINDO AÍ",
+        "description": "FROMSOFTWARE anuncia seu novo sucessor da série SoulsLike. Elden Ring vem aí e promete ser estar no pódio do GOTY.",
+        "photoCover": "https://criticalhits.com.br/wp-content/uploads/2021/12/jujutsu-kaisen-0-movie-poster-1.jpg"
+    },
+    
+
+    
+
+];
